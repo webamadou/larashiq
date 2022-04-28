1e2ebf9629b5ded6a272c05bcde3c5c3148f4fbe
Amadou BA <paab26@live.fr>
2022-04-28 20:15:08 +0000

    Initial commit

---
+++ resources/lang/en/user_form_terms.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'login' => 'Login',
+    'register' => 'Register',
+    'email' => 'Email',
+    'password' => 'Password',
+    'remember_me' => 'Remember me',
+    'forgot_password' => 'Forgot your password'
+];
---
+++ resources/lang/fr/user_form_terms.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'login' => 'Connexion',
+    'register' => 'S\'inscrire',
+    'email' => 'E-mail',
+    'password' => 'Mot de passe',
+    'remember_me' => 'Se Souvenir de moi',
+    'forgot_password' => 'Mot de passe oublié'
+];
